add398fdeb9a892e09842fad0b66618f8954bec9
Arjun Venkatraman <arjun@mojolab.org>
2018-02-16 15:05:37 +0530

    Updated

--- lib/livdatdatastream.py
+++ lib/livdatdatastream.py
@@ -1,36 +1,52 @@
 import os,sys,pandas,json
 class DataStream(object):
-	def __init__(self,path,columnnames=[],latestcount=100):
+	def __init__(self,path,columnnames=["created_at"],latestcount=50):
 		self.path=path
 		self.columnnames=columnnames
+		self.latestcount=latestcount
 		if not os.path.exists(self.path):
 			os.mkdir(self.path)
 		self.latestjsonpath=os.path.join(self.path,"latest.json")
 		self.fulljsonpath=os.path.join(self.path,"full.json")
+		self.fulldf=pandas.DataFrame(columns=self.columnnames)
+		self.latestdf=pandas.DataFrame(columns=self.columnnames)
 		
-		if not os.path.exists(self.fulljsonpath):
-				
-			self.fulldf=pandas.DataFrame(columns=self.columnnames)
-			self.latestdf=pandas.DataFrame(columns=self.columnnames)
-		else:		
-			self.fulldf=pandas.read_json(self.fulljsonpath,orient="index").drop_duplicates().sort_values(by="created_at")
-			self.latestdf=self.fulldf[-1*latestcount]
+		if os.path.exists(self.fulljsonpath):
+			try:
+				self.fulldf=pandas.read_json(self.fulljsonpath,orient="index").drop_duplicates().sort_values(by="created_at")
+				self.latestdf=self.fulldf[-1*self.latestcount:]
+			except ValueError as exception:
+				if "need more than 0 values to unpack" in exception:
+					print "No entries in datastream yet"
+			
 		self.columnnames=list(self.fulldf.columns)
 		
 		with open(self.fulljsonpath,"w") as f:
-			f.write(json.dumps(self.fulldf.to_json(orient="index")))
+			try:
+				f.write(self.fulldf.to_json(orient="index"))
+				f.write("\n")
+			except Exception as exception:
+				print exception
 		with open(self.latestjsonpath,"w") as f:
-			f.write(json.dumps(self.latestdf.to_json(orient="index")))
-	
+			try:
+				f.write(self.latestdf.to_json(orient="index"))
+				f.write("\n")
+			except Exception as exception:
+				print exception
 				
 	def save_stream(self):
+		print "Saving stream at " + self.path
 		with open(self.fulljsonpath,"w") as f:
-			f.write(json.dumps(self.fulldf.to_json(orient="index")))
+			#print self.fulldf
+			f.write(self.fulldf.to_json(orient="index"))
+			f.write("\n")
 		with open(self.latestjsonpath,"w") as f:
-			f.write(json.dumps(self.latestdf.to_json(orient="index")))
-		
+			#print self.latestdf
+			f.write(self.latestdf.to_json(orient="index"))
+			f.write("\n")
+	
 	def append_data(self,newdatadf):
-		self.fulldf=self.fulldf.append(newdatadf).drop_duplicates().sort_values(by="created_at")
-		self.latestdf=self.fulldf[-1*latestcount]
+		self.fulldf=self.fulldf.append(newdatadf).sort_values(by="created_at",ascending=False).drop_duplicates('created_at').sort_index()
+		self.latestdf=self.fulldf[-1*self.latestcount:]
 		
 	
